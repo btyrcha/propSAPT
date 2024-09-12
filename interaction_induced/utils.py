29c3a84158bce9dfe4b4f3f63c3650cef6e2a2f8
btyrcha <bartektyrcha123@gmail.com>
2024-09-12 15:20:14 +0200

    fix: pass returns in `trace_memory_peak` wrapper

--- interaction_induced/utils.py
+++ interaction_induced/utils.py
@@ -9,11 +9,16 @@ def trace_memory_peak(func):
 
     def wrapper(*args, **kwargs):
         tracemalloc.start()
-        func(*args, **kwargs)
+
+        result = func(*args, **kwargs)
         _, peak_size = tracemalloc.get_traced_memory()
+
         psi4.core.print_out(f"\nFunciton: {func}\n")
         psi4.core.print_out(f"...peak meamory usage: {peak_size*1e-6:.2f} MB\n")
+
         tracemalloc.reset_peak()
         tracemalloc.stop()
+
+        return result
 
     return wrapper
