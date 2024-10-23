696cd46c9615acf3aa88283f16aebe28798c768d
btyrcha <bartektyrcha123@gmail.com>
2024-10-23 15:22:51 +0200

    bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = [
 
 setup(
     name="interaction-induced",
-    version="0.1.0",
+    version="0.2.0",
     author="Bartosz Tyrcha",
     author_email="bartektyrcha123@gmail.com",
     description="Package for calculations of first-order interaction-induced properties and density matrices in the spirit of SAPT.",
