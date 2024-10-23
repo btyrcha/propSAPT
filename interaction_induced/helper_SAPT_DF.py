e924f6bd008a86d5dfe068a12629c3e2043d359e
btyrcha <bartektyrcha123@gmail.com>
2024-10-23 15:01:29 +0200

    license info

--- interaction_induced/helper_SAPT_DF.py
+++ interaction_induced/helper_SAPT_DF.py
@@ -1,6 +1,8 @@
 """
 Modified version of helper_SAPT.py form Psi4Numpy repository.
-Contains helper classes and functions for SAPT with MO density-fitting in Psi4NumPy.
+Contains helper classes and functions for SAPT with MO density-fitting.
+
+Modified by: Bartosz Tyrcha
 
 Original authors and license info:
 __authors__ = "Daniel G. A. Smith"
@@ -13,6 +15,38 @@ __date__ = "2015-12-01"
 References:
 - Equations and algorithms from [Szalewicz:2005:43], [Jeziorski:1994:1887],
 [Szalewicz:2012:254], and [Hohenstein:2012:304]
+
+License:
+Copyright (c) 2014-2018, The Psi4NumPy Developers.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+       copyright notice, this list of conditions and the following
+       disclaimer in the documentation and/or other materials provided
+       with the distribution.
+
+    * Neither the name of the Psi4NumPy Developers nor the names of any
+       contributors may be used to endorse or promote products derived
+       from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import time
