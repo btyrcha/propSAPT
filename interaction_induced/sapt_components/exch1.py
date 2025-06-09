544082e0a1d69606bf0b9c0af5d03a967bb39415
btyrcha <bartektyrcha123@gmail.com>
2025-06-09 17:15:56 +0200

    exch1 Sinf with JK

--- interaction_induced/sapt_components/exch1.py
+++ interaction_induced/sapt_components/exch1.py
@@ -1,6 +1,8 @@
+import numpy as np
 import opt_einsum as oe
 
 from ..molecule import Dimer
+from ..utils import CalcTimer
 
 
 def calc_exch1_s2_energy(dimer: Dimer):
@@ -36,32 +38,27 @@ def calc_exch1_energy(dimer: Dimer):
         _type_: First-order SAPT exchange energy with Sinfinity.
     """
 
-    ###  Matrices and tensors from helper_dimer
-    s_ab = dimer.s("ab")
-    s_ba = dimer.s("ba")
-    s_sa = dimer.s("sa")
-    s_rb = dimer.s("rb")
-
     ### Exch100_Sinfinity
-    exch1_sinf = -2 * oe.contract(
-        "Bb,aB,sa,bs", dimer.A_bb, s_ab, s_sa, dimer.omegaA_bs
-    )
-    exch1_sinf -= 2 * oe.contract(
-        "Aa,bA,rb,ar", dimer.B_aa, s_ba, s_rb, dimer.omegaB_ar
-    )
-    exch1_sinf -= 2 * oe.contract(
-        "Bb,Aa,sA,rB,Qar,Qbs", dimer.A_bb, dimer.B_aa, s_sa, s_rb, dimer.Qar, dimer.Qbs
-    )
-    exch1_sinf += 4 * oe.contract(
-        "BD,AC,Da,Cb,sA,rB,Qar,Qbs",
-        dimer.A_bb,
-        dimer.B_aa,
-        s_ba,
-        s_ab,
-        s_sa,
-        s_rb,
-        dimer.Qar,
-        dimer.Qbs,
-    )
+    with CalcTimer("Exch1_Sinf energy calculation"):
+
+        _, K1 = dimer.compute_jk(
+            dimer.orbitals["r"],
+            dimer.orbitals["b"],
+            tensor=dimer.I_rb,
+        )
+        J2, _ = dimer.compute_jk(
+            dimer.orbitals["s"],
+            dimer.orbitals["b"],
+            tensor=dimer.F_sb,
+        )
+
+        exch1_sinf = -2 * oe.contract("sb,bs", dimer.F_sb, dimer.omegaA_bs)
+        exch1_sinf -= 2 * oe.contract("ra,ar", dimer.E_ra, dimer.omegaB_ar)
+        exch1_sinf -= 2 * np.trace(
+            dimer.orbitals["s"] @ dimer.J_sa @ dimer.orbitals["a"].T @ K1
+        )
+        exch1_sinf += 4 * np.trace(
+            dimer.orbitals["r"] @ dimer.E_ra @ dimer.orbitals["a"].T @ J2
+        )
 
     return exch1_sinf
