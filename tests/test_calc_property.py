847ea7c77d5ebdf06deebb97ed158d61a1424ed1
btyrcha <bartektyrcha123@gmail.com>
2025-07-18 15:31:11 +0200

    feature: x2_exch-ind,r_S2 and x2_exch-disp_S2 in dipole test

--- tests/test_calc_property.py
+++ tests/test_calc_property.py
@@ -14,8 +14,8 @@ def test_calc_dipole(prepare_dimer):
         "x1_exch,r": -1.581842e-02,
         "x2_ind,r": 1.042533e-03,
         "x2_disp": 2.195424e-03,
-        # "x2_exch-ind,r_S2": -2.096264e-03,
-        # "x2_exch-disp_S2": -3.902404e-04,
+        "x2_exch-ind,r_S2": -2.096264e-03,
+        "x2_exch-disp_S2": -3.902404e-04,
     }
 
     results = calc_property(prepare_dimer, property_type)
