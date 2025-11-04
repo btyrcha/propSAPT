3e508bee0fc1da1c4acad8f4cf91f2343b327c4d
btyrcha <bartektyrcha123@gmail.com>
2025-11-04 14:31:48 +0100

    refactor: rename variables for clarity in get_exch_ind_s2_density function

--- prop_sapt/densities_components/exch_ind.py
+++ prop_sapt/densities_components/exch_ind.py
@@ -49,7 +49,7 @@ def get_exch_ind_s2_density(mol: Dimer, monomer: str) -> np.ndarray:
     s_rb = mol.s("rb")
     s_br = s_rb.T
 
-    not_u_ra = (
+    theta_t_ra = (
         -oe.contract("sr,ab,bs->ra", s_sr, s_ab, mol.omegaA_bs)
         + oe.contract("ba,cb,ar->rc", s_ba, s_ab, mol.omegaB_ar)
         - oe.contract("br,cb,ac->ra", s_br, s_rb, mol.omegaB_ar)
@@ -57,9 +57,9 @@ def get_exch_ind_s2_density(mol: Dimer, monomer: str) -> np.ndarray:
         + oe.contract("sa,cb,Qar,Qbs->rc", s_sa, s_ab, mol.Qar, mol.Qbs)
         - oe.contract("sr,cb,Qac,Qbs->ra", s_sr, s_rb, mol.Qar, mol.Qbs)
     )
-    not_u_ra = mol.cpscf("A", perturbation=not_u_ra.T)
+    theta_t_ra = mol.cpscf("A", perturbation=theta_t_ra.T)
 
-    not_u_sb = (
+    theta_t_sb = (
         +oe.contract("ba,ac,cs->sb", s_ba, s_ab, mol.omegaA_bs)
         - oe.contract("as,ca,bc->sb", s_as, s_sa, mol.omegaA_bs)
         - oe.contract("rs,ba,ar->sb", s_rs, s_ba, mol.omegaB_ar)
@@ -67,7 +67,7 @@ def get_exch_ind_s2_density(mol: Dimer, monomer: str) -> np.ndarray:
         + oe.contract("ba,rc,Qar,Qcs->sb", s_ba, s_rb, mol.Qar, mol.Qbs)
         - oe.contract("rs,ca,Qar,Qbc->sb", s_rs, s_sa, mol.Qar, mol.Qbs)
     )
-    not_u_sb = mol.cpscf("B", perturbation=not_u_sb.T)
+    theta_t_sb = mol.cpscf("B", perturbation=theta_t_sb.T)
 
     rho_MO_exch_ind = np.zeros((mol.nmo, mol.nmo))
 
@@ -174,9 +174,9 @@ def get_exch_ind_s2_density(mol: Dimer, monomer: str) -> np.ndarray:
                 "cs,da,sb,Qrc,Qbd->ra", s_rs, s_sa, mol.get_cpscf_sb(), mol.Qrr, mol.Qbs
             )
             # < V P2 R([V, R(X)]) > + < V P2 R([X, R(V)]) >
-            + oe.contract("rR,Ra->ra", mol.omegaB_rr, not_u_ra)
-            - oe.contract("Aa,rA->ra", mol.omegaB_aa, not_u_ra)
-            + 2 * oe.contract("Qar,Qbs,sb->ra", mol.Qar, mol.Qbs, not_u_sb)
+            + oe.contract("rR,Ra->ra", mol.omegaB_rr, theta_t_ra)
+            - oe.contract("Aa,rA->ra", mol.omegaB_aa, theta_t_ra)
+            + 2 * oe.contract("Qar,Qbs,sb->ra", mol.Qar, mol.Qbs, theta_t_sb)
             # < V P2 R(X) R(V) >
             + oe.contract(
                 "br,ac,sb,cs->ra", s_br, s_ab, mol.get_cpscf_sb(), mol.omegaA_bs
@@ -242,16 +242,16 @@ def get_exch_ind_s2_density(mol: Dimer, monomer: str) -> np.ndarray:
 
         rho_MO_exch_ind_ra = mol.cpscf(
             "A", perturbation=rho_MO_exch_ind_ra.T
-        ) + oe.contract("rsab,bs->ra", mol.t_rsab, not_u_sb.T)
+        ) + oe.contract("rsab,bs->ra", mol.t_rsab, theta_t_sb.T)
 
         rho_MO_exch_ind[mol.slices["r"], mol.slices["a"]] = rho_MO_exch_ind_ra
         rho_MO_exch_ind[mol.slices["a"], mol.slices["r"]] = rho_MO_exch_ind_ra.T
 
         rho_MO_exch_ind[mol.slices["a"], mol.slices["a"]] = -oe.contract(
-            "rA,ar->aA", mol.get_cpscf_ra(), not_u_ra.T
+            "rA,ar->aA", mol.get_cpscf_ra(), theta_t_ra.T
         )
         rho_MO_exch_ind[mol.slices["r"], mol.slices["r"]] = oe.contract(
-            "ra,aR->rR", mol.get_cpscf_ra(), not_u_ra.T
+            "ra,aR->rR", mol.get_cpscf_ra(), theta_t_ra.T
         )
 
     if monomer == "B":
@@ -357,9 +357,9 @@ def get_exch_ind_s2_density(mol: Dimer, monomer: str) -> np.ndarray:
                 "rc,da,cb,Qar,Qsd->sb", s_rs, s_sa, mol.get_cpscf_sb(), mol.Qar, mol.Qss
             )
             # < V P2 R([V, R(X)]) > + < V P2 R([X, R(V)]) >
-            + oe.contract("sS,Sb->sb", mol.omegaA_ss, not_u_sb)
-            - oe.contract("Bb,sB->sb", mol.omegaA_bb, not_u_sb)
-            + 2 * oe.contract("Qar,Qbs,ra->sb", mol.Qar, mol.Qbs, not_u_ra)
+            + oe.contract("sS,Sb->sb", mol.omegaA_ss, theta_t_sb)
+            - oe.contract("Bb,sB->sb", mol.omegaA_bb, theta_t_sb)
+            + 2 * oe.contract("Qar,Qbs,ra->sb", mol.Qar, mol.Qbs, theta_t_ra)
             # < V P2 R(X) R(V) >
             + oe.contract(
                 "br,ac,ra,cs->sb", s_br, s_ab, mol.get_cpscf_ra(), mol.omegaA_bs
@@ -425,16 +425,16 @@ def get_exch_ind_s2_density(mol: Dimer, monomer: str) -> np.ndarray:
 
         rho_MO_exch_ind_sb = mol.cpscf(
             "B", perturbation=rho_MO_exch_ind_sb.T
-        ) + oe.contract("rsab,ar->sb", mol.t_rsab, not_u_ra.T)
+        ) + oe.contract("rsab,ar->sb", mol.t_rsab, theta_t_ra.T)
 
         rho_MO_exch_ind[mol.slices["s"], mol.slices["b"]] = rho_MO_exch_ind_sb
         rho_MO_exch_ind[mol.slices["b"], mol.slices["s"]] = rho_MO_exch_ind_sb.T
 
         rho_MO_exch_ind[mol.slices["b"], mol.slices["b"]] = -oe.contract(
-            "sB,bs->bB", mol.get_cpscf_sb(), not_u_sb.T
+            "sB,bs->bB", mol.get_cpscf_sb(), theta_t_sb.T
         )
         rho_MO_exch_ind[mol.slices["s"], mol.slices["s"]] = oe.contract(
-            "sb,bS->sS", mol.get_cpscf_sb(), not_u_sb.T
+            "sb,bS->sS", mol.get_cpscf_sb(), theta_t_sb.T
         )
 
     return rho_MO_exch_ind
